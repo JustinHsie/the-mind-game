7385fe52f6529d21e62672f95cbea1a58e837f38
Justin Hsie <justinhsie1001@gmail.com>
2021-03-07 01:39:08 -0800

    Delete misc

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './views/App';
+import { Game } from './views/Game';
 
 import 'primereact/resources/themes/md-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -8,4 +8,4 @@ import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('root'));
